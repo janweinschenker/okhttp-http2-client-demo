648d36e3833643b8892cbb7cd897a66f4354b557
Jan Weinschenker <jan.weinschenker@holisticon.de>
2017-04-11 09:04:34 +0200

    chore(logging) log message to demonstrate asynchronous execution

--- src/main/java/de/holisticon/jdk9/App.java
+++ src/main/java/de/holisticon/jdk9/App.java
@@ -10,17 +10,17 @@ import java.util.concurrent.TimeUnit;
  * Hello world!
  */
 public class App {
-    // In order to run this, you need the alpn-boot-XXX.jar in the bootstrap classpath.
-    private static final Logger LOG = Logger.getLogger(App.class);
+  // In order to run this, you need the alpn-boot-XXX.jar in the bootstrap classpath.
+  private static final Logger LOG = Logger.getLogger(App.class);
 
-    public static void main(String[] args) throws Exception {
-      App app = new App();
+  public static void main(String[] args) throws Exception {
+    App app = new App();
 
-      app.startRequest();
-      app.startAsyncRequest();
+    app.startRequest();
+    app.startAsyncRequest();
 
-      System.exit(0);
-    }
+    System.exit(0);
+  }
 
 
   private void startRequest() throws InterruptedException {
@@ -39,48 +39,43 @@ public class App {
       LOG.debug("After " + duration + " seconds: " + response.body().string());
 
     } catch (IOException e) {
-     LOG.error("IOException", e);
+      LOG.error("IOException", e);
     }
   }
-
 
 
   private void startAsyncRequest() throws InterruptedException {
     LOG.debug("======================================= Start asynchronous request");
     OkHttpClient client = getUnsafeOkHttpClient();
+
     Request request = new Request.Builder()
         .url("https://localhost:8443/greeting?name=JavaLand") // The Http2Server should be running here.
         .build();
-    long startTime = System.nanoTime();
-    for (int i = 0; i < 3; i++) {
-      Thread.sleep(1000); // http://stackoverflow.com/questions/32625035/when-using-http2-in-okhttp-why-multi-requests-to-the-same-host-didnt-use-just
-      client.newCall(request).enqueue(new Callback() {
-        @Override
-        public void onFailure(Call call, IOException e) {
-          e.printStackTrace();
-        }
 
-        @Override
-        public void onResponse(Call call, Response response) throws IOException {
-          long duration = TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - startTime);
-          LOG.debug("Protocol version: " + response.protocol());
-          LOG.debug("After " + duration + " seconds: " + response.body().string());
+    client.newCall(request).enqueue(new Callback() {
+      @Override
+      public void onFailure(Call call, IOException e) {
+        LOG.error("IOException", e);
+      }
 
-        }
-
-      });
-    }
-
+      @Override
+      public void onResponse(Call call, Response response) throws IOException {
+        LOG.debug("Protocol version: " + response.protocol());
+        LOG.debug("content: " + response.body().string());
+      }
+    });
+    LOG.debug("request created!!!");
+    Thread.sleep(5000);
   }
 
-    private OkHttpClient getUnsafeOkHttpClient() {
-        try {
-            OkHttpClient okHttpClient = new OkHttpClient.Builder()
-                .build();
+  private OkHttpClient getUnsafeOkHttpClient() {
+    try {
+      OkHttpClient okHttpClient = new OkHttpClient.Builder()
+          .build();
 
-            return okHttpClient;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+      return okHttpClient;
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
+  }
 }
